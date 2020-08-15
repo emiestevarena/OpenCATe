89f966f39605bb18c905a2ab091911a11065eacd
emiestevarena <65693639+emiestevarena@users.noreply.github.com>
2020-08-15 11:40:03 -0300

    Create main.cs
    The starting point!

---
+++ cs/main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Open CAT beta");
+    String source;
+    String target;
+    Console.WriteLine("Insert source text");
+    source=Console.ReadLine();
+    Console.WriteLine("Insert target text");
+    target=Console.ReadLine();
+  }
+}
