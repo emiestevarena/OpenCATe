c79be3a0181c0793afe73d24fc9b708ac7820db9
emiestevarena <65693639+emiestevarena@users.noreply.github.com>
2020-08-23 13:17:24 -0300

    Update Source.java
    reads source file

--- classes/Source.java
+++ classes/Source.java
@@ -1,23 +1,33 @@
 package classes;
 import java.io.File;
 import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class Source{
   private String sText;
   private int length;
 
-  public Source sourceIn(){
-    File sFile = new File("source.txt");
-    Scanner scSource = new Scanner(sFile);
-    this.length=0;
-    this.text="";
-    while(sc.hasNextLine()){
-      this.sText+=(sc.nextLine());
-      this.length++;
+  public void ReadFile(){
+    try {
+      File myObj = new File("source.txt");
+      Scanner myReader = new Scanner(myObj);
+      this.sText="";
+      this.length=0;
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+        this.sText+=data;
+        this.length++;
+      } 
+      myReader.close();
+      System.out.println("segments: "+this.length);
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
     }
-    System.out.println("segments: "+this.length);
-    return sFile;
   }
+
 
   public String getSText(){
     return sText;
